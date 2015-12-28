e2e6e2109fadbd55fc0dd1be995985f07866d97f
Marianne Rentchler <rentchlerm@eisti.eu>
2015-12-28 12:16:06 +0100

    creation dune figure en 2d

--- systeme.ml
+++ systeme.ml
@@ -1,31 +1,60 @@
-(*Première méthode pour créer un agent *)
+#load "graphics.cma";;
+open Graphics;;
 
-type agent =
-{
-	mutable startState : state
-}
-and transition = Condition of (agent -> bool)
-				|Event of (agent -> bool)
-				|ConditionOr of (agent -> bool)
-				|ConditionAnd of (agent -> bool)
-				|ConditionXor of (agent -> bool)
-				|ConditionNor of (agent -> bool)
-and state = 
-{
-	mutable parentstate : state option;
-	mutable substate : state option;
-	mutable transitions : state list;
-	mutable begin_action : state -> state;
-	mutable action : state -> state;
-	mutable end_action : state -> state
-}
+type etat = { mutable x : float; mutable y : float; mutable visee : float; mutable leve : bool };;
+let crayon = { x = 0.0; y = 0.0; visee = 0.0; leve = false };;
+open_graph "";;
 
-(*Seconde méthode : en considérant l'agent comme un objet*)
-let o =
-	object(self)
-		val message = "hello world!"
-		method hello = print_endline message
-		initializer
-			self#hello
+let round x = if x >= 0.0 then int_of_float (x +. 0.5) else int_of_float (x -. 0.5);;
+
+let fixe_crayon b = crayon.leve <- b;;
+
+let pi_sur_180 = let pi = 4.0 *. (atan 1.0) in pi /. 180.0;;
+
+let tourne angle = crayon.visee <- (crayon.visee +. angle *. pi_sur_180);;
+
+
+let avance d =
+	let dx = d *. cos (crayon.visee)
+	and dy = d *. sin (crayon.visee) in
+	crayon.x <- crayon.x +. dx;
+	crayon.y <- crayon.y +. dy;
+	if crayon.leve
+	then moveto (round crayon.x) (round crayon.y)
+	else lineto (round crayon.x) (round crayon.y);;
+
+let couleur_du_trace = black;;
+
+let couleur_du_fond = white;;
+
+let zero_x = float_of_int ((size_x ()) / 2);;
+
+let zero_y = float_of_int ((size_y ()) / 2);;
+
+let vide_ecran () =
+set_color couleur_du_fond;
+fill_rect 0 0 (size_x ()) (size_y ());
+set_color couleur_du_trace;
+crayon.x <- zero_x;
+crayon.y <- zero_y;
+crayon.visee <- 0.0;
+crayon.leve <- false;
+moveto (round crayon.x) (round crayon.y);;
+
+let rec motif n c =
+if n = 0 then avance c else
+	begin
+	motif (n-1) (c /. 3.0); tourne 60.0;
+	motif (n-1) (c /. 3.0); tourne (-120.0);
+	motif (n-1) (c /. 3.0); tourne 60.0;
+	motif (n-1) (c /. 3.0)
 	end;;
-o#hello 
+let flocon n c = for i = 1 to 3 do motif n c; tourne (-120.0) done;;
+
+vide_ecran ();;
+flocon 4 100.0;;
+
+(*
+type agent = {x: float; y : float; z :float};;
+
+let relation a = {x=10.*.(a.x-.a.y); y=(10.*.a.x)-.(a.x*.a.z)-.a.y; z=(a.x*.a.y)-.(8./.3.)*.a.z};;*)
