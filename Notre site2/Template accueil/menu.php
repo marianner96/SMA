a608c8cb07ef8a9c1bc1aa4579d5ffa4c211ee8b
Marianne Rentchler <rentchlerm@eisti.eu>
2016-04-01 08:57:40 +0200

    ok

--- Notre site2/Template accueil/menu.php
+++ Notre site2/Template accueil/menu.php
@@ -1,24 +1,20 @@
 <nav id="nav">
 	<ul>
 		<li><a class="icon fa-home" href="index.html"><span>Accueil</span></a></li>
-		<li><a class="icon fa-retweet" href="right-sidebar.html"><span>Explications</span></a></li>
-		<li>
-			<a href="#" class="icon fa-bar-chart-o"><span>Simulation</span></a>
+		<li><a class="icon fa-bars" href="right-sidebar.html"><span>Explications</span></a>
 			<ul>
-				<li><a href="#">Lorem ipsum dolor</a></li>
-				<li><a href="#">Magna phasellus</a></li>
-				<li><a href="#">Etiam dolore nisl</a></li>
-				<li>
-					<a href="#">Phasellus consequat</a>
+				<li><a href="#">La théorie du chaos</a>
 					<ul>
-						<li><a href="#">Magna phasellus</a></li>
-						<li><a href="#">Etiam dolore nisl</a></li>
-						<li><a href="#">Phasellus consequat</a></li>
+						<li><a href="#">Un peu d'histoire</a></li>
+						<li><a href="#">La théorie du chaos</a></li>
+						<li><a href="#">Equations de Lorenz</a></li>
 					</ul>
 				</li>
-				<li><a href="#">Veroeros feugiat</a></li>
+				<li><a href="#">Les systèmes dynamiques</a></li>
+				<li><a href="#">Notre système multi-agents</a></li>
 			</ul>
 		</li>
-		<li><a class="icon fa-cog" href="left-sidebar.html"><span>Sources</span></a></li>
+		<li><a href="#" class="icon fa-bar-chart-o"><span>Simulation</span></a></li>
+		<li><a class="icon fa-book" href="left-sidebar.html"><span>Sources</span></a></li>
 	</ul>
 </nav>
