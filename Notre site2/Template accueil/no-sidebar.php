2563b85d305fde0cdc64832dba53727714e43139
Marianne Rentchler <rentchlerm@eisti.eu>
2016-04-04 12:02:00 +0200

    remplissage de laccueil

--- Notre site2/Template accueil/no-sidebar.php
+++ Notre site2/Template accueil/no-sidebar.php
@@ -10,7 +10,7 @@
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
-		<link rel="stylesheet" href="assets/css/essai.css" />
+		<link rel="stylesheet" href="assets/css/main.css" />
 		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
 	</head>
 	<body class="no-sidebar">
@@ -21,7 +21,7 @@
 					<div id="header" class="container">
 
 						<!-- Logo -->
-							<h1 id="logo"><a href="index.html">Théorie du chaos</a></h1>
+							<h1 id="logo">Théorie du chaos</h1>
 							<p>Projet de TIPE de deuxième année de classe préparatoire à l'EISTI Pau</p>
 
 						<?php include("menu.php"); ?>
@@ -33,10 +33,61 @@
 				<div id="main-wrapper">
 					<div id="main" class="container">
 						<div id="content">
-							Coucou ça marche !
-
+							Nous sommes deux élèves en classe préparatoire à l' EISTI Pau, ce site est notre projet de TIPE.
+							Ce site représente un projet sur deux sur la théorie du chaos. Sur la première année, nous avons réalisé
+							un rapport bibliographique sur cette théorie. Puis lors de la deuxième année, nous avons fait un poster 
+							scientifique, un cahier des charges pour l'application informatique que nous allions développer et enfin 
+							cette application informatique qui content un sysyème multi-agent dont le code (en OCaml) est disponible. 
+							Lors de la première année et du premier semestre de la deuxième année, nous étions 3.
 						</div>
 					</div>
+				</div>
+
+				<div id="features-wrapper">
+					<section id="features" class="container">
+						<div class="row">
+							<div clas="4u 12u(mobile)">
+								<section>
+									<a href="theorie-du-chaos1.pdf" class="image featured">
+										<img src="image3.jpg" width="100px;" height="100px;" alt></img>
+									</a>
+									<header>
+										<h3>Rapport bibliographique</h3>
+									</header>
+									<p>Le résultat de nos recherches de première année sont résulées dans ce document.</p>
+								</section>
+							</div>
+							<div class="4u 12u(mobile)">
+								<section>
+									<a href="theorie-du-chaos.pdf" class="image featured">
+										<img src="image.jpeg" alt></img>
+									</a>
+									<header>
+										<h3>Poster scientifique</h3>
+									</header>
+									<p>
+										C'est le poster scientique que nous avons réalisé pendant le premier semestre 
+										de la deuxième année.
+									</p>
+								</section>
+							</div>
+							<div class="4u 12u(mobile)">
+								<section>
+									<a href="cahier-des-charges.pdf" class="image featured">
+										<img src="image2.jpg" width="100px;" height="150px;" alt></img>
+									</a>
+									<header>
+										<h3>Cahier des charges</h3>
+									</header>
+									<p>
+										Le cahier des charges que nous avons réalisé en parallèle du poster scientifique 
+										contient toute la description de notre projet et nos attentes envers l'application 
+										scientique que nous avons réalisé.
+									</p>
+								</section>
+							</div>
+						</div> 
+					</section> 
 				</div>
 
 		<!-- Scripts -->
