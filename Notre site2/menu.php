ad2576fe7993af65348fcb87c674ac71a8f9e0f8
Marianne Rentchler <rentchlerm@eisti.eu>
2016-05-12 14:43:32 +0200

    remise des pages quon avait perdu...

---
+++ Notre site2/menu.php
@@ -0,0 +1,20 @@
+<nav id="nav">
+	<ul>
+		<li><a class="icon fa-home" href="accueil.php"><span>Accueil</span></a></li>
+		<li><a class="icon fa-bars" href="#"><span>Explications</span></a>
+			<ul>
+				<li><a href="#">La théorie du chaos</a>
+					<ul>
+						<li><a href="histoire.php">Un peu d'histoire</a></li>
+						<li><a href="effetpap.php">L'effet papillon</a></li>
+						<li><a href="equations.php">Equations de Lorenz</a></li>
+					</ul>
+				</li>
+				<li><a href="#">Les systèmes dynamiques</a></li>
+				<li><a href="#">Notre système multi-agents</a></li>
+			</ul>
+		</li>
+		<li><a class="icon fa-bar-chart-o" href="simulation.php"><span>Simulation</span></a></li>
+		<li><a class="icon fa-book" href="sources.php"><span>Sources</span></a></li>
+	</ul>
+</nav>
